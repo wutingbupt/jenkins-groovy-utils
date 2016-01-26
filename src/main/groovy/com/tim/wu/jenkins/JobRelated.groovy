5ca8bc8be828d0c2c66a7342871553b9af3fc8f8
etigwuu <tim.t.wu@ericsson.com>
2016-01-26 21:28:35 +0100

    Add jenkins dependency and a simple disbale job  method

--- src/main/groovy/com/tim/wu/jenkins/JobRelated.groovy
+++ src/main/groovy/com/tim/wu/jenkins/JobRelated.groovy
@@ -1,11 +1,23 @@
 package com.tim.wu.jenkins
 
+import hudson.model.AbstractProject
+import jenkins.model.Jenkins
+
 /**
  * Created by etigwuu on 2016/1/26.
  */
 class JobRelated {
 
+    /**
+     * Disables a job based on the name
+     *
+     * @param name Job name
+     * @return true if the job is in disabled status, return false otherwise
+     */
     def disableJob(String name){
-
+        def result = Jenkins.instance.getItemByFullName(name)
+        //TODO Seems work flow dont follow this
+        result.disable()
+        return result.isDisabled()
     }
 }
